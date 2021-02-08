cebb1374eee964bd7d2d2c9f17ea5c1053e4114a
Ofeoritse nanna <nannaofe20@gmail.com>
2021-02-08 17:51:46 -0600

    inital commit

---
+++ src/main/java/com/galvanize/speedway/SpeedwayTrialsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.speedway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedwayTrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedwayTrialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/speedway/SpeedwayTrialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.speedway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedwayTrialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
