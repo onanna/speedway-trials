f131f79cedc2b84ff3f27adffdd599a7c665ff46
Vyom <vyom.reloaded@gmail.com>
2021-02-10 18:42:19 -0500

    updated service unit test to include mocked repositories

--- src/main/java/com/galvanize/speedway/entity/Racecar.java
+++ src/main/java/com/galvanize/speedway/entity/Racecar.java
@@ -1,25 +1,102 @@
 package com.galvanize.speedway.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Racecar {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
 
-	
-	
+	private String nickName;
+
+	private String model;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JsonIgnore
+	private Driver driver;	
+
+	private boolean available;
+
+	private int topSpeed;
+
+	private String type;
+
+	private int year;
+
+	public Racecar() {
+
+	}
+
+	public Racecar(String nickName, String model, Driver driver, boolean available, int topSpeed, String type,
+			int year) {
+		super();
+		this.nickName = nickName;
+		this.model = model;
+		this.driver = driver;
+		this.available = available;
+		this.topSpeed = topSpeed;
+		this.type = type;
+		this.year = year;
+		//this.owner = driver.getId();
+	}
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public Driver getDriver() {
+		return driver;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public int getTopSpeed() {
+		return topSpeed;
+	}
+
+	public int getTopSpeedInKph() {
+		return (int) Math.round(topSpeed * 1.6);
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int getYear() {
+		return year;
+	}
+	
+	public Long getOwner() {
+		return this.driver.getId();
+	}
+	
+	public void setDriver(Driver driver) {
+		this.driver = driver;
+	}
+	
 	
 
 }
--- src/test/java/com/galvanize/speedway/service/RacecarServiceTest.java
+++ src/test/java/com/galvanize/speedway/service/RacecarServiceTest.java
@@ -1,8 +1,12 @@
 package com.galvanize.speedway.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,37 +15,59 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.speedway.entity.Driver;
 import com.galvanize.speedway.entity.Racecar;
 import com.galvanize.speedway.repository.RacecarRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class RacecarServiceTest {
-	
+
 	RacecarService racecarService;
-	
+
 	@Mock
 	RacecarRepository racecarRepo;
-	
+
 	@BeforeEach
 	public void init() {
 		racecarService = new RacecarService(racecarRepo);
 	}
-	
-	
+
 	@Test
-	public void test_createRacecars() {
-		when(racecarRepo.save(Mockito.any(Racecar.class))).thenReturn(addRacecarId(new Racecar()));
-		
+	public void test_createRacecars() throws JsonParseException, JsonMappingException, IOException {
+
+		Racecar racecar = generateRacecar();
+		Driver driver = generateDriver();
+		racecar.setDriver(driver);
+
+		when(racecarRepo.save(Mockito.any(Racecar.class))).thenReturn(racecar);
+
 		Racecar actualRacecar = racecarService.addRacecar(new Racecar());
-		
+
 		assertNotNull(actualRacecar.getId());
-		
+		assertEquals(43, actualRacecar.getId());
+		assertEquals("The Condor", actualRacecar.getNickName());
+		assertEquals("Corvette", actualRacecar.getModel());
+		assertEquals(2019, actualRacecar.getYear());
+		assertEquals(332, actualRacecar.getOwner());
+
 		verify(racecarRepo).save(Mockito.any(Racecar.class));
 	}
-	
-	private Racecar addRacecarId(Racecar racecar) {
-		racecar.setId(1l);
+
+	private Racecar generateRacecar() throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		Racecar racecar = mapper.readValue(new File("src/test/resources/racecar.json"), Racecar.class);
 		return racecar;
+
+	}
+
+	private Driver generateDriver() throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		Driver driver = mapper.readValue(new File("src/test/resources/driver.json"), Driver.class);
+		return driver;
+
 	}
 
 }
